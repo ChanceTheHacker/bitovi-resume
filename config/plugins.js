b3f5fa541199ea7b9209e2c49178d046539b3790
William <ertyjunior@gmail.com>
2019-12-14 20:50:33 -0500

    added icon

--- config/plugins.js
+++ config/plugins.js
@@ -16,13 +16,13 @@ module.exports = [
   {
     resolve: `gatsby-plugin-manifest`,
     options: {
-      name: `gatsby-starter-default`,
-      short_name: `starter`,
+      name: `resume`,
+      short_name: `resume`,
       start_url: `/`,
       background_color: `#663399`,
       theme_color: `#663399`,
       display: `minimal-ui`,
-      icon: `src/images/svg/chelseaIcon/chelseaIcon.svg`, // This path is relative to the root of the site.
+      icon: `src/images/icon.png`, // This path is relative to the root of the site.
     },
   },
   {
